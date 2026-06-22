1f3e3966a04109367af1f8fc8fb715b0bd18e060
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-06-22 16:20:49 -0400

    Add project files.

---
+++ LogicBuilder.App.KendoGrid.Bsl.Business/Requests/KendoGridDataRequest.cs
@@ -0,0 +1,12 @@
+﻿using LogicBuilder.Expressions.Utils.ExpansionDescriptors;
+
+namespace LogicBuilder.App.KendoGrid.Bsl.Business.Requests
+{
+    public class KendoGridDataRequest
+    {
+        public string? DataType { get; set; }
+        public string? ModelType { get; set; }
+        public KendoGridDataSourceRequestOptions? Options { get; set; }
+        public SelectExpandDefinitionDescriptor? SelectExpandDefinition { get; set; }
+    }
+}
---
+++ LogicBuilder.App.KendoGrid.Bsl.Business/Requests/KendoGridDataSourceRequestOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace LogicBuilder.App.KendoGrid.Bsl.Business.Requests
+{
+    public class KendoGridDataSourceRequestOptions
+    {
+        public string? Aggregate { get; set; }
+        public string? Filter { get; set; }
+        public string? Group { get; set; }
+        public int Page { get; set; }
+        public string? Sort { get; set; }
+        public int PageSize { get; set; }
+    }
+}
